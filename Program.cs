ac97f3f1a5943291487499dfd3c6c122e7de4534
alica kavuljakova <drobeek@gmail.com>
2023-06-07 07:35:31 -0700

    new app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+using WeatherApp;
+
+string key = File.ReadAllText("appsettings.json");
+string apiKey= JObject.Parse(key).GetValue("APIKey").ToString();
+Console.WriteLine("What is your zipcode?");
+var zipCode = Console.ReadLine();
+
+
+string url = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={apiKey}";
+Console.WriteLine($"It is currently: {WeatherMap.GetTemp(url)} degrees Farenheit in {WeatherMap.GetCity(url)}");
---
+++ WeatherMap.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class WeatherMap
+    {
+        public static double GetTemp(string url)
+        {
+            var client=new HttpClient();
+          var response=  client.GetStringAsync(url).Result;
+            var temp= double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+          return  temp;
+            
+        }
+        public static string GetCity(string url)
+        {
+        var client= new HttpClient();
+            string response = client.GetStringAsync(url).Result;
+            string city = JObject.Parse(response)["name"].ToString();
+            return city;
+        }
+
+
+    }
+}
